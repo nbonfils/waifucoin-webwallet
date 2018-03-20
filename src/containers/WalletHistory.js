8110763420ab853eb629ec360196616da40a1ffd
nbonfils <nils.bonfils@epfl.ch>
2018-03-20 11:52:42 +0100

    Finish first UI prototype

--- src/containers/WalletHistory.js
+++ src/containers/WalletHistory.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {Table} from 'antd';
+
 /**
  * WalletHistory displays the history of transaction of this wallet
  */
@@ -9,6 +11,49 @@ export default class WalletHistory extends React.Component {
    * @return {Component} A table with a transaction by row
    */
   render() {
-    return null;
+    let renderCol = (text, data) => {
+      let name = data.sent ? 'tr-sent' : 'tr-rcv';
+      return <span className={name}>{text}</span>;
+    };
+
+    const columns = [{
+      title: 'Amount',
+      dataIndex: 'amount',
+      width: 100,
+      render: renderCol,
+    }, {
+      title: 'Adress',
+      dataIndex: 'adress',
+      render: renderCol,
+    }, {
+      title: 'Date',
+      dataIndex: 'date',
+      render: renderCol,
+    }];
+
+    const data = [{
+      amount: 234,
+      adress: '0xwfc0a76c10023cf2241aa',
+      date: '01/03/18',
+      sent: true,
+    }, {
+      amount: 27,
+      adress: '0xwfc0a76c10023cf2241aa',
+      date: '29/02/18',
+      sent: true,
+    }, {
+      amount: 10,
+      adress: '0xwfc0a76c10023cf2241aa',
+      date: '26/02/18',
+      sent: false,
+    }];
+
+    return (
+      <Table
+        title={() => 'Transaction History'}
+        columns={columns}
+        dataSource={data}
+      />
+    );
   }
 }
